045a744fa8c6d4c73ce11516a8196e4500473c2b
Marek Prochazka <m.prochazka2002@gmail.com>
2021-07-05 16:39:34 +0200

    person create action base

--- backend/core/models/person.py
+++ backend/core/models/person.py
@@ -4,3 +4,8 @@ from django.contrib.auth.models import User
 
 class Person(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    @staticmethod
+    def create_person_from_user(user: User):
+        person = Person(user=user)
+        person.save()
