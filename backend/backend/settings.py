2d4358f07116959ef9f92fed5a78b0a65e7148e2
Marek Prochazka <m.prochazka2002@gmail.com>
2021-07-08 19:55:39 +0200

    disable csrf

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -49,7 +49,8 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
+    # removed csrf protection for at least now
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
