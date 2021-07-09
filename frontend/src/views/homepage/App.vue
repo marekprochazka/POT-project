7533c084a048a2b9527ed8464ea15346d6a242eb
Marek Prochazka <m.prochazka2002@gmail.com>
2021-07-09 20:09:34 +0200

    add plan button

--- frontend/src/views/homepage/App.vue
+++ frontend/src/views/homepage/App.vue
@@ -5,6 +5,7 @@
     <PlanTile plan-title="Léto 2021" trainings-snippet="PO-bench, ÚT-back, ST-legs"></PlanTile>
     <PlanTile plan-title="Léto 2021" trainings-snippet="PO-bench, ÚT-back, ST-legs"></PlanTile>
     <h2 style="color: wheat">user:{{$store.state.username}}</h2>
+    <Icon class="homepage__plusIcon" icon-type="plus"></Icon>
   </div>
 
 </template>
@@ -12,10 +13,12 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import PlanTile from './components/PlanTile/App.vue'
+import Icon from '@/views/utils/icon/App.vue'
+
 
 export default defineComponent({
   name: 'vue-homepage',
-  components: {PlanTile}
+  components: {PlanTile, Icon}
 })
 </script>
 
