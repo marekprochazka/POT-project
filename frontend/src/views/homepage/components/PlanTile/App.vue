60698bf29009d2d451aa3c4046a655bcf1ed67e8
Marek Prochazka <m.prochazka2002@gmail.com>
2021-07-18 19:31:42 +0200

    plan tile menu overlay

--- frontend/src/views/homepage/components/PlanTile/App.vue
+++ frontend/src/views/homepage/components/PlanTile/App.vue
@@ -1,26 +1,40 @@
 <template>
   <div class="planTile__container">
-    <div class="planTile__innerContainer">
-      <h3 class="planTile__title">{{planTitle}}</h3>
-      <p class="planTile__trainingSnippet">{{trainingsSnippet}}</p>
-      <Icon class="planTile__icon-menu" icon-type="menu" />
-      <Icon class="planTile__icon-add" icon-type="add" />
-    </div>
+    <transition name="planTile__menuSwitch-fade-out">
+
+      <div v-if="!isMenuOpen" class="planTile__innerItemsContainer">
+        <h3 class="planTile__title">{{ planTitle }}</h3>
+        <p class="planTile__trainingSnippet">{{ trainingsSnippet }}</p>
+        <Icon @click.prevent="isMenuOpen = !isMenuOpen" class="planTile__icon-menu" icon-type="menu"/>
+        <Icon class="planTile__icon-add" icon-type="add"/>
+      </div>
+    </transition>
+
+    <transition name="planTile__menuSwitch-fade-in">
+      <div v-if="isMenuOpen" class="planTile__innerMenuContainer">
+        <Icon @click.prevent="isMenuOpen = !isMenuOpen" class="planTile__icon-menu" icon-type="menu"/>
+      </div>
+    </transition>
 
 
   </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import {defineComponent} from 'vue';
 import Icon from '@/views/utils/icon/App.vue'
 
 export default defineComponent({
   name: 'vue-plan-tile',
-  props: {
-    planTitle: {type:String, required:true},
-    trainingsSnippet: {type:String, required:true}
+  data() {
+    return {
+      isMenuOpen: false,
+    }
   },
-  components:{Icon}
+  props: {
+    planTitle: {type: String, required: true},
+    trainingsSnippet: {type: String, required: true}
+  },
+  components: {Icon}
 })
 </script>
