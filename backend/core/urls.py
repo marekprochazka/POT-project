6187de70c77b7f5a6fc6a7a402c8871ca5595dd8
Marek Prochazka <m.prochazka2002@gmail.com>
2021-07-31 18:38:51 +0200

    training create endpoint

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -3,15 +3,20 @@ from core.views.sign_in import SignInView
 from core.views.logout import LogoutView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from core.views.plan import PlansListView, PlanCreateView, PlanDetailView, PlanDeleteView
+from core.views.training import TrainingCreateView
 
 app_name = 'core_api'
 
 urlpatterns = [
     path('login', TokenObtainPairView.as_view(), name='login'),
     path('sign-in', SignInView.as_view(), name='sing_in'),
-    path('plan/list', PlansListView.as_view(), name='plan_list'),
     path('logout', LogoutView.as_view(), name='logout'),
+
+    path('plan/list', PlansListView.as_view(), name='plan_list'),
     path('plan/create', PlanCreateView.as_view(), name='plan_create'),
     path('plan/delete/<str:plan_id>', PlanDeleteView.as_view(), name='plan_delete'),
     path('plan/detail/<str:plan_id>', PlanDetailView.as_view(), name='plan_detail'),
+
+    path('training/create/<str:plan_id>', TrainingCreateView.as_view(), name='training_create')
+
 ]
