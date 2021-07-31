14ccac044d5b872f72847910c3e8128a94c93516
Marek Prochazka <m.prochazka2002@gmail.com>
2021-07-31 17:35:33 +0200

    removed plan tile subtitle

--- frontend/src/views/homepage/components/PlanTile/App.vue
+++ frontend/src/views/homepage/components/PlanTile/App.vue
@@ -1,28 +1,14 @@
 <template>
   <div class="planTile__container">
-    <transition name="planTile__menuSwitch-fade-out">
-
       <div v-if="!isMenuOpen" class="planTile__innerItemsContainer">
         <h3 class="planTile__title">{{ planTitle }}</h3>
         <p class="planTile__trainingSnippet">{{ trainingsSnippet }}</p>
-        <Icon @click.prevent="isMenuOpen = !isMenuOpen" class="planTile__icon-menu" icon-type="menu"/>
-        <Icon class="planTile__icon-add" icon-type="add"/>
       </div>
-    </transition>
-
-    <transition name="planTile__menuSwitch-fade-in">
-      <div v-if="isMenuOpen" class="planTile__innerMenuContainer">
-        <Icon @click.prevent="isMenuOpen = !isMenuOpen" class="planTile__icon-menu" icon-type="menu"/>
-      </div>
-    </transition>
-
-
   </div>
 </template>
 
 <script>
 import {defineComponent} from 'vue';
-import Icon from '@/views/utils/icon/App.vue'
 
 export default defineComponent({
   name: 'vue-plan-tile',
@@ -35,6 +21,5 @@ export default defineComponent({
     planTitle: {type: String, required: true},
     trainingsSnippet: {type: String, required: true}
   },
-  components: {Icon}
 })
 </script>
