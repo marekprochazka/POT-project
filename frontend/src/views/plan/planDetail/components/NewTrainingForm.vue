72a909d55b8b621c4178bbc00d8a7d5bcf09cd7e
Marek Prochazka <m.prochazka2002@gmail.com>
2021-08-14 16:39:10 +0200

    textfield scss

--- frontend/src/views/plan/planDetail/components/NewTrainingForm.vue
+++ frontend/src/views/plan/planDetail/components/NewTrainingForm.vue
@@ -1,6 +1,6 @@
 <template>
   <h3>This is gonna be new training form...</h3>
-  <vue-text-field v-model="test" type="text" :rules="rules"/>
+  <vue-text-field v-model="test" type="text" :rules="rules" label="text"/>
   <p style="color:white;">{{ test }}</p>
 </template>
 
--- frontend/src/views/utils/TextField/App.vue
+++ frontend/src/views/utils/TextField/App.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="textField__container">
-    <input v-bind="$attrs" :type="type" v-model="input">
-    <div class="textField__errors" v-if="isError">
-      <p v-for="error in errorsList" :key="error" style="color:red;">{{ error }}</p>
+  <div class="form__group form-group px-10">
+    <label v-if="title" :class="labelClasses"  :for="label">{{label}}</label>
+    <input :class="inputClasses" v-bind="$attrs" :type="type" v-model="input" :id="label" :placeholder="label">
+    <div class="form__errors" v-if="isError">
+      <p v-for="error in errorsList" :key="error" class="form__error px-10 mt-05">{{ error }}</p>
     </div>
   </div>
 
@@ -16,6 +17,8 @@ export default defineComponent({
   props: {
     inputValue: {type: String, default: ''},
     type: {required: true, type: String},
+    label: {required: true, type: String},
+    title: {required:false,type:Boolean, default:true},
     rules: {required: false, type: Array as PropType<Array<(value: string) => string | boolean>>, default: () => []}
   },
   data() {
@@ -35,6 +38,12 @@ export default defineComponent({
         this.innerValue = val
         this.$emit('input', val)
       }
+    },
+    inputClasses(): Array<string> {
+      return this.isError ? ['form__input', 'form-control', 'form__input--Error'] : ['form__input', 'form-control']
+    },
+    labelClasses(): Array<string> {
+      return this.isError ? ['form__text', 'mb-05', 'px-10', 'form__text--Error'] : ['form__text', 'mb-05', 'px-10']
     }
   },
   methods: {
